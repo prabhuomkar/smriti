f933e92b8ad1e75da4649aabdca323943e592cb9
Omkar Prabhu <prabhuomkar@pm.me>
2022-09-24 17:01:06 +0530

    Feature unit test

--- api/internal/handlers/features_test.go
+++ api/internal/handlers/features_test.go
@@ -1,7 +1,27 @@
 package handlers
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+
+	"github.com/labstack/echo"
+)
 
 func TestGetFeatures(t *testing.T) {
-
+	tests := []Test{
+		{
+			"get features",
+			"/v1/features",
+			"/v1/features",
+			nil,
+			nil,
+			func(handler *Handler) func(ctx echo.Context) error {
+				return handler.GetFeatures
+			},
+			http.StatusOK,
+			`{"favourites":false,"hidden":false,"trash":false,"albums":false,` +
+				`"explore":false,"places":false,"things":false,"people":false,"sharing":false}`,
+		},
+	}
+	executeTests(t, tests)
 }
