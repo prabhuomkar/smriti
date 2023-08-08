3c09e79694da3b0ead1f075e89a5b93d30e7b6d7
Omkar Prabhu <prabhuomkar@pm.me>
2023-08-08 21:54:35 +0530

    some more unit test coverage

--- worker/tests/components/test_faces.py
+++ worker/tests/components/test_faces.py
@@ -6,6 +6,7 @@ import grpc
 
 from src.components.faces import Faces
 from src.protos.api_pb2_grpc import APIStub
+from src.protos.api_pb2 import GetUsersResponse, MediaItemFaceEmbeddingsResponse, MediaItemFaceEmbedding, MediaItemEmbedding
 
 
 @mock.patch('src.providers.faces.PyTorchModule.__init__', return_value=None)
@@ -33,3 +34,56 @@ async def test_faces_process_failed_process_exception(_):
     result = await faces.process('mediaitem_user_id', 'mediaitem_id', None,
                     {'previewPath': 'location/to-preview-file'})
     assert result == {'previewPath':'location/to-preview-file'}
+
+
+@mock.patch('src.providers.faces.PyTorchModule.__init__', return_value=None)
+@mock.patch('src.components.Faces._grpc_save_mediaitem_people', return_value=None)
+@pytest.mark.asyncio
+async def test_faces_cluster_success(_, __):
+    api_stub_mock = mock.MagicMock()
+    api_stub_mock.GetUsers.return_value = GetUsersResponse(users=['user-id'])
+    api_stub_mock.GetMediaItemFaceEmbeddings.return_value = MediaItemFaceEmbeddingsResponse(
+        mediaItemFaceEmbeddings=[
+            MediaItemFaceEmbedding(id='face-id-1', mediaItemId='mediaitem-id-1', peopleId='', embedding=MediaItemEmbedding(embedding=[0.42,0.64])),
+            MediaItemFaceEmbedding(id='face-id-2', mediaItemId='mediaitem-id-2', peopleId='people-id-1', embedding=MediaItemEmbedding(embedding=[0.41,0.64])),
+            MediaItemFaceEmbedding(id='face-id-3', mediaItemId='mediaitem-id-3', peopleId='', embedding=MediaItemEmbedding(embedding=[3.02,4.64])),
+            MediaItemFaceEmbedding(id='face-id-4', mediaItemId='mediaitem-id-4', peopleId='', embedding=MediaItemEmbedding(embedding=[3.42,4.24])),
+            MediaItemFaceEmbedding(id='face-id-5', mediaItemId='mediaitem-id-5', peopleId='', embedding=MediaItemEmbedding(embedding=[33.42,41.24])),
+        ]
+    )
+    faces = Faces(api_stub_mock, 'pytorch', ['0.9', 'vggface2'])
+    faces.cluster()
+
+@mock.patch('src.providers.faces.PyTorchModule.__init__', return_value=None)
+@mock.patch('src.components.Faces._grpc_save_mediaitem_people', return_value=None)
+@pytest.mark.asyncio
+async def test_faces_cluster_success_no_embeddings(_, __):
+    api_stub_mock = mock.MagicMock()
+    api_stub_mock.GetUsers.return_value = GetUsersResponse(users=['user-id'])
+    api_stub_mock.GetMediaItemFaceEmbeddings.return_value = MediaItemFaceEmbeddingsResponse(mediaItemFaceEmbeddings=[])
+    faces = Faces(api_stub_mock, 'pytorch', ['0.9', 'vggface2'])
+    faces.cluster()
+
+@mock.patch('src.providers.faces.PyTorchModule.__init__', return_value=None)
+@pytest.mark.asyncio
+async def test_faces_cluster_failed_cluster_exception(_):
+    api_stub_mock = mock.MagicMock()
+    api_stub_mock.GetUsers.return_value = GetUsersResponse(users=['user-id'])
+    api_stub_mock.GetMediaItemFaceEmbeddings.side_effect = grpc.RpcError(Exception('some error'))
+    faces = Faces(api_stub_mock, 'pytorch', ['0.9', 'vggface2'])
+    faces.cluster()
+
+@mock.patch('src.providers.faces.PyTorchModule.__init__', return_value=None)
+@pytest.mark.asyncio
+async def test_faces_cluster_failed_grpc_exception(_):
+    api_stub_mock = mock.MagicMock()
+    api_stub_mock.GetUsers.return_value = GetUsersResponse(users=['user-id'])
+    api_stub_mock.GetMediaItemFaceEmbeddings.return_value = MediaItemFaceEmbeddingsResponse(
+        mediaItemFaceEmbeddings=[
+            MediaItemFaceEmbedding(id='face-id-1', mediaItemId='mediaitem-id-1', peopleId='', embedding=MediaItemEmbedding(embedding=[0.42,0.64])),
+            MediaItemFaceEmbedding(id='face-id-2', mediaItemId='mediaitem-id-2', peopleId='people-id-1', embedding=MediaItemEmbedding(embedding=[0.41,0.64])),
+        ]
+    )
+    api_stub_mock.SaveMediaItemPeople.side_effect = grpc.RpcError(Exception('some error'))
+    faces = Faces(api_stub_mock, 'pytorch', ['0.9', 'vggface2'])
+    faces.cluster()
