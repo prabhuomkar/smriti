7c13b1056641b573cffb3efe0623320d4a05c337
Omkar Prabhu <prabhuomkar@pm.me>
2023-08-26 21:14:51 +0530

    Support for arm devices with updated OCR

--- worker/src/providers/ocr/paddlepaddle.py
+++ worker/src/providers/ocr/paddlepaddle.py
@@ -1,38 +1,55 @@
 """OCR: Paddle"""
 import logging
 
+import cv2
 from moviepy.editor import VideoFileClip
-from paddleocr import PaddleOCR
+import fastdeploy as fd
 
-
-logging.getLogger().handlers = []
 
 class PaddleModule:
     """PaddleModule OCR"""
 
     def __init__(self, params: dict) -> None:
-        self.model = PaddleOCR(show_log=False, use_angle_cls=True, lang='en',
-                               det_model_dir=f'/models/ocr/{params["det_model_dir"]}',
-                               rec_model_dir=f'/models/ocr/{params["rec_model_dir"]}',
-                               cls_model_dir=f'/models/ocr/{params["cls_model_dir"]}')
+        self.default_option = fd.RuntimeOption()
+        self.default_option.use_ort_backend()
+        self.det_model = fd.vision.ocr.DBDetector(
+            model_file=f'/models/ocr/{params["det_model_dir"]}/model.onnx',
+            runtime_option=self.default_option,
+            model_format=fd.ModelFormat.ONNX,
+        )
+        self.cls_model = fd.vision.ocr.Classifier(
+            model_file=f'/models/ocr/{params["cls_model_dir"]}/model.onnx',
+            runtime_option=self.default_option,
+            model_format=fd.ModelFormat.ONNX,
+        )
+        self.rec_model = fd.vision.ocr.Recognizer(
+            model_file=f'/models/ocr/{params["rec_model_dir"]}/model.onnx',
+            label_path=f'/models/ocr/{params["rec_model_dir"]}/en_dict.txt',
+            runtime_option=self.default_option,
+            model_format=fd.ModelFormat.ONNX,
+        )
+        self.model = fd.vision.ocr.PPOCRv3(
+            det_model=self.det_model,
+            cls_model=self.cls_model,
+            rec_model=self.rec_model,
+        )
 
     def extract(self, mediaitem_user_id: str, mediaitem_id: str, mediaitem_type: str, input_file: str) -> dict:
         """Extract text from mediaitem"""
         result, words = [], []
         if mediaitem_type == 'photo':
-            result = self.model.ocr(input_file)
+            result = self.model.predict(cv2.imread(input_file))
+            if result is not None:
+                words = result.text
         else:
             video_clip = VideoFileClip(input_file)
             for frame in video_clip.iter_frames(fps=video_clip.fps):
-                _result = self.model.ocr(frame)
-                result += _result
+                _result = self.model.predict(frame)
+                if _result is not None:
+                    result += _result.text
             video_clip.reader.close()
-        for res in result:
-            for line in res:
-                if line[1][1] > 0.90:
-                    words += line[1][0].split()
+            words = result
         logging.debug(f'extracted text for user {mediaitem_user_id} mediaitem {mediaitem_id}: {words}')
-
         if len(words) == 0:
             return None
 
